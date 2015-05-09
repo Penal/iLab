7951a13db5bd9912699ea4d2ffb35ce133962608
RoZa <roman.zavodskikh@phystech.edu>
2015-05-09 19:36:52 +0300

    Added what() to ++Stack

--- ++Stack/main.cpp
+++ ++Stack/main.cpp
@@ -2,14 +2,22 @@
 
 int main (void)
 {
-	Stack<int> test_stack(5);
-	for (int i = 0; i < 11; i++)
-	{
-	    test_stack.push(i+1);
-	}
-	for (int i = 0; i < 13; i++)
-	{
-	    test_stack.pop();
-	}
+    try
+    {
+        Stack<int> test_stack;
+        for (int i = 0; i < 11; i++)
+        {
+	        test_stack.push(i+1);
+        }
+	    for (int i = 0; i < 12; i++)
+        {
+    	    test_stack.pop();
+        }
+    }
+    catch (Stack_crushed exc)
+    {
+        //Here should be actions to retrieve the exception... But I don't know what to do, actually
+        cerr << "ATTENTION!!! STACK CRUSHED!!! PROGRAM HAS LOST ITS STABILITY!!!\n";
+    }
 	return 0;
 }
--- ++Stack/stack.h
+++ ++Stack/stack.h
@@ -1,76 +1,79 @@
 ﻿#include <iostream>
 #include <cstring>
 #include <cassert>
-using namespace std;
+
+using std::ostream;
+using std::cerr;
+using std::bad_alloc;
+using std::endl;
+using std::string;
+using std::exception;
 
 const int WRONG_NUM = 0;
 const int STD_STACK_SIZE = 10;
 const int STACK_NUMBER_DIGITS = 9;
 const int STACK_MEMBER_WIDTH = 20;
+const int RETURN_EXC_CODE = 1;
 enum STACK_ERRNO_VALUES
 { 
 	STACK_IS_OK = 0, 
 	STACK_OVERFLOW,
 	STACK_UNDERFLOW,
-	STACK_DATA_PTR_NULL
+	STACK_DATA_PTR_NULL,
+    STACK_IMPOSSIBLE_TO_FIX
 };
 
-class Stack_exception
+class Stack_exception : public exception
 {
 public:
 	Stack_exception():state_code_(STACK_IS_OK){};
 	Stack_exception(int code):state_code_(code){};
 	~Stack_exception(){};
-	int get_state_code();
-private:
+	inline int get_state_code() const noexcept;
+    const char* what() const noexcept;
+protected:
 	int state_code_;
+};
+
+class Stack_crushed : public Stack_exception
+{
+public:
+    Stack_crushed() {this->state_code_ = STACK_IMPOSSIBLE_TO_FIX;};
 };
 
 
 template <class T>
 class Stack {
 public:
-	Stack();
-	Stack(int size);
+	Stack(int size, ostream& ostr);
 	~Stack();
 	int not_ok();
-	int dump(ostream& ostr);
+	int dump(ostream& ostr) const noexcept;
 	int push(const T value);
 	T pop();
-	template <class R> friend ostream& operator<<(ostream& ostr, const Stack<R>& stack);
-	std::string strerror(int code);
+	template <class R> friend ostream& operator<<(ostream& ostr, const Stack<R>& stack) noexcept;
 	int react(Stack_exception exception);
 private:
 	T* data_;
 	int count_;
 	int size_;
 	int stack_errno_;
+    ostream& stack_stream_;
 };
 
-template <class T> Stack<T>::Stack():
-	data_(new T[STD_STACK_SIZE]), count_(0), size_(STD_STACK_SIZE), stack_errno_(STACK_IS_OK)
+template <class T> Stack<T>::Stack(int size = STD_STACK_SIZE, ostream& ostr = cerr):
+	data_(nullptr), count_(0), size_(size), stack_errno_(STACK_IS_OK), stack_stream_(ostr)
 {
 	try
 	{
-		assert(data_);
+        data_ = new T[size];
 		memset(data_, 0, size_*sizeof(T));
 		this->not_ok();
 	}
-	catch (Stack_exception exception)
-	{
-		this->react(exception);
-	}
-}
-
-template <class T> Stack<T>::Stack(int size):
-	data_(new T[size]), count_(0), size_(size), stack_errno_(STACK_IS_OK)
-{
-	try
-	{
-		assert(data_);
-		memset(data_, 0, size_*sizeof(T));
-		this->not_ok();
-	}
+    catch (bad_alloc)
+    {
+        throw Stack_exception(STACK_DATA_PTR_NULL);
+    }
 	catch (Stack_exception exception)
 	{
 		this->react(exception);
@@ -84,6 +87,7 @@ template <class T> Stack<T>::~Stack()
 	count_ = WRONG_NUM;
 	size_ = WRONG_NUM;
 } 
+
 template <class T> int Stack<T>::not_ok() 
 {
 	if (!data_)
@@ -109,10 +113,9 @@ template <class T> int Stack<T>::not_ok()
 	return stack_errno_;
 }
 
-template <class T> int Stack<T>::dump(ostream& ostr) 
+template <class T> int Stack<T>::dump(ostream& ostr) const noexcept
 {
-	using namespace std;
-	ostr << "Stack [" << this << "] [" << "" << this->strerror(this->stack_errno_) << "]" << endl;
+	ostr << "Stack [" << this << "]" << endl;
 	ostr << "--->Count = " << count_ << ", Size = " << size_ << endl; 
 	ostr << "--->Data[" << data_ << "]" << endl;
 	if (data_)
@@ -148,6 +151,7 @@ template <class T> int Stack<T>::push(const T value)
 	catch (Stack_exception exception)
 	{
 		this->react(exception);
+        return RETURN_EXC_CODE;
 	}
 }
 
@@ -165,44 +169,45 @@ template <class T> T Stack<T>::pop()
 	catch (Stack_exception exception)
 	{
 		this->react(exception);
+        return RETURN_EXC_CODE;
 	}
 }
 
-template <class T> std::string Stack<T>::strerror(int code)
+const char* Stack_exception::what() const noexcept
 {
-	std::string rtr_val = "";
-	switch (code)
+	switch (this->state_code_)
 	{
-		case STACK_IS_OK: rtr_val = "Stack is OK";				break;
-		case STACK_OVERFLOW: rtr_val = "Stack overflow";			break;
-		case STACK_UNDERFLOW: rtr_val = "Stack underflow";			break;
-		case STACK_DATA_PTR_NULL: rtr_val = "Stack data null pointer";		break;
+		case STACK_IS_OK: return "Stack is OK"; 			                           	break;
+		case STACK_OVERFLOW: return "Stack overflow";	    	                      	break;
+		case STACK_UNDERFLOW: return "Stack underflow"; 		                    	break;
+		case STACK_DATA_PTR_NULL: return "Stack data null pointer"; 		            break;
+        case STACK_IMPOSSIBLE_TO_FIX: return "It\'s impossible to fix this stack!!!";   break;
 	}
-
-	return rtr_val;
 }
 
 
-template <class T> ostream& operator<< (ostream& ostr, Stack<T>& stack)
+template <class T> ostream& operator<< (ostream& ostr, Stack<T>& stack) noexcept
 {
 	stack.dump(ostr);
 	return ostr;
 }
 
-template <class T> int Stack<T>::react(Stack_exception exception)
+template <class T> int Stack<T>::react(Stack_exception exc)
 {
 	T* old_data = NULL;
 	T* new_data = NULL;
 
+    this->stack_stream_ << exc.what() << endl;
 
-	switch (exception.get_state_code())
+	switch (exc.get_state_code())
 	{
-		case STACK_IS_OK: 	cerr << "The stack is OK, this message mustn't be written unless exceptions are developed wrong" << endl;
-					cerr << "Okay, I'll give you a dump, to be on the safe side" << endl << this;
+		case STACK_IS_OK: 	
+                    this->stack_stream_ << "The stack is OK, this message mustn't be written unless exceptions are developed wrong" << endl;
+					this->stack_stream_ << "Okay, I'll give you a dump, to be on the safe side" << endl << this;
 		break;
 		case STACK_OVERFLOW:
-					cerr << "There was an stack overflow, but I have an algorythm to fix it. It\' all right!!!" << endl;
-					cerr << "Here\'s the dump for you to feel safe" << endl;
+					this->stack_stream_ << "There was an stack overflow, but I have an algorythm to fix it. It\'s all right!!!" << endl;
+					this->stack_stream_ << "Here\'s the dump for you to feel safe" << endl;
 					old_data = data_;
 					new_data = new T[size_*2];
 					assert(new_data);
@@ -216,26 +221,29 @@ template <class T> int Stack<T>::react(Stack_exception exception)
 					delete [] old_data;
 					data_ = new_data;
 					size_ *= 2;
-					cerr << *this;
+					this->stack_stream_ << *this;
 		break;
 		case STACK_UNDERFLOW:
-					cerr << "There\'s stack underflow. You mustn\'t let it REPEAT AGAIN!!! TAKE THE DUMP!!! FIX IT!!!";
-					cerr << endl;
-					cerr << *this;
+					this->stack_stream_ << "There\'s stack underflow. You mustn\'t let it REPEAT AGAIN!!! TAKE THE DUMP!!! FIX IT!!!";
+					this->stack_stream_ << endl;
+					this->stack_stream_ << *this;
+                    throw Stack_crushed();
 		break;
 		case STACK_DATA_PTR_NULL:
-					cerr << "The data pointer is null in your stack. Here it is to make it clear:" << endl << *this;
+					this->stack_stream_ << "The data pointer is null in your stack. Here it is to make it clear:" << endl << *this;
+                    throw Stack_crushed();
 		break;
 		default:
-					cerr << "The error only donkey can have!!! There is some unpredictable state. It\'s code is ";
-					cerr << exception.get_state_code() << endl;
+					this->stack_stream_ << "The error only donkey can have!!! There is some unpredictable state. It\'s code is ";
+					this->stack_stream_ << exc.get_state_code() << endl;
+                    throw Stack_crushed();
 		break;
 	}
 
 	return 0;
 }
 
-int Stack_exception::get_state_code()
+inline int Stack_exception::get_state_code() const noexcept
 {
 	return this->state_code_;
 }
